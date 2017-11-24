6b9124581ad0700f1014627ad8dcd8eb1849b9f8
HuangLi <huangliop@163.com>
2017-11-24 17:49:48 +0800

    装饰器插件放最前面

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,10 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
+    //decorator,要放最前面
+    config=injectBabelPlugin('transform-decorators-legacy',config);
     //add antd-mobile
     config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
-    //decorator
-    config=injectBabelPlugin('transform-decorators-legacy',config);
     //正式环境下用cheap-module-source-map
     if(env === "production")config.devtool='cheap-module-source-map';
     return config;
