d4647ad61ba2c6c409ffd35bb56ae7c45059b3a4
HuangLi <huangliop@163.com>
2017-11-29 17:48:50 +0800

    添加格式插件

--- README.md
+++ README.md
@@ -61,51 +61,6 @@ App构建出发布的文件到 `build` 目录.<br>
 如果你先自定义webpack的配置，请编辑config-overrides.js。因为使用了react-app-rewired插件所以不需要执行 eject命令。
 具体使用方法请常见 [https://github.com/timarney/react-app-rewired](https://github.com/timarney/react-app-rewired)
 
-
-## Debugging in the Editor
-
-**This feature is currently only supported by [Visual Studio Code](https://code.visualstudio.com) and [WebStorm](https://www.jetbrains.com/webstorm/).**
-
-Visual Studio Code and WebStorm support debugging out of the box with Create React App. This enables you as a developer to write and debug your React code without leaving the editor, and most importantly it enables you to have a continuous development workflow, where context switching is minimal, as you don’t have to switch between tools.
-
-### Visual Studio Code
-
-You would need to have the latest version of [VS Code](https://code.visualstudio.com) and VS Code [Chrome Debugger Extension](https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome) installed.
-
-Then add the block below to your `launch.json` file and put it inside the `.vscode` folder in your app’s root directory.
-
-```json
-{
-  "version": "0.2.0",
-  "configurations": [{
-    "name": "Chrome",
-    "type": "chrome",
-    "request": "launch",
-    "url": "http://localhost:3000",
-    "webRoot": "${workspaceRoot}/src",
-    "userDataDir": "${workspaceRoot}/.vscode/chrome",
-    "sourceMapPathOverrides": {
-      "webpack:///src/*": "${webRoot}/*"
-    }
-  }]
-}
-```
->Note: the URL may be different if you've made adjustments via the [HOST or PORT environment variables](#advanced-configuration).
-
-Start your app by running `npm start`, and start debugging in VS Code by pressing `F5` or by clicking the green debug icon. You can now write code, set breakpoints, make changes to the code, and debug your newly modified code—all from your editor.
-
-### WebStorm
-
-You would need to have [WebStorm](https://www.jetbrains.com/webstorm/) and [JetBrains IDE Support](https://chrome.google.com/webstore/detail/jetbrains-ide-support/hmhgeddbohgjknpmjagkdomcpobmllji) Chrome extension installed.
-
-In the WebStorm menu `Run` select `Edit Configurations...`. Then click `+` and select `JavaScript Debug`. Paste `http://localhost:3000` into the URL field and save the configuration.
-
->Note: the URL may be different if you've made adjustments via the [HOST or PORT environment variables](#advanced-configuration).
-
-Start your app by running `npm start`, then press `^D` on macOS or `F9` on Windows and Linux or click the green debug icon to start debugging in WebStorm.
-
-The same way you can debug your application in IntelliJ IDEA Ultimate, PhpStorm, PyCharm Pro, and RubyMine. 
-
 ## Formatting Code Automatically
 
 Prettier is an opinionated code formatter with support for JavaScript, CSS and JSON. With Prettier you can format the code you write automatically to ensure a code style within your project. See the [Prettier's GitHub page](https://github.com/prettier/prettier) for more information, and look at this [page to see it in action](https://prettier.github.io/prettier/).
--- src/stores/RootStore.js
+++ src/stores/RootStore.js
@@ -1,89 +1,99 @@
-import Agent from '../transport-layer/Agent'; 
+import Agent from '../transport-layer/Agent';
 import PersistData from './PersistData';
-import ResponseCode from '../transport-layer/ResponseCode'; 
+import ResponseCode from '../transport-layer/ResponseCode';
 
 import UserStore from './UserStore';
-import UIStore from './UIStore'; 
+import UIStore from './UIStore';
 
- class RootStore{
-    constructor(){
-        this.agent=new Agent();
-        const persistData=new PersistData();
-        this.userStore=new UserStore(this,persistData);
-        this.UIStore=new UIStore(this,persistData); 
-    }
-    /**
-     * 发送POST请求
-     * @param {*请求地址} url 
-     * @param {*参数} params 
-     * @param {*不显示loading图像} noLoading
-     */
-    sendPost(url,_params,noLoading){
-        const params=this._buildParams(_params);
-        return this.agent.post(url,params,noLoading).then(json=>this._handleData(json,url,params))
-    }
-    /**
-     * 发生GET请求
-     * @param {*请求地址} url 
-     * @param {*参数} params 
-     * @param {*不显示loading图像} noLoading
-     */
-    sendGet(url,_params,noLoading){
-        const params=this._buildParams(_params);
-        return this.agent.get(url,params,noLoading).then(json=>this._handleData(json,url,params))
-    }
-    /**
-     * 显示提示信息，默认3秒
-     * @param {*提示内容} msg 
-     */
-    showToast(msg){
-       this.agent.showToast(msg);
-    }
-    /**
-     * 显示加载框，期间不可操作
-     * @param {*下面显示的提示内容，默认‘加载中’} msg 
-     */
-    showLoading(msg){
-        this.agent.showLoading(msg);
-    }
-    hideLoading(){
-        this.agent.hideLoading();
-    }
-    /**
-     * 构建参数
-     * @param {*参数} par 
-     */
-    _buildParams(par){
-        const params={...par};
-        //某些接口希望不传token，就需要删除token字段，默认都带token
-        if(params.noToken){
-            delete params.noToken;
-            delete params.token;
-        }else{
-            /*如果没有this.UIStore.token就不代token，
+class RootStore {
+  constructor() {
+    this.agent = new Agent();
+    const persistData = new PersistData();
+    this.userStore = new UserStore(this, persistData);
+    this.UIStore = new UIStore(this, persistData);
+  }
+  /**
+   * 发送POST请求
+   * @param {*请求地址} url
+   * @param {*参数} params
+   * @param {*不显示loading图像} noLoading
+   */
+  sendPost(url, _params, noLoading) {
+    const params = this._buildParams(_params);
+    return this.agent
+      .post(url, params, noLoading)
+      .then(json => this._handleData(json, url, params));
+  }
+  /**
+   * 发生GET请求
+   * @param {*请求地址} url
+   * @param {*参数} params
+   * @param {*不显示loading图像} noLoading
+   */
+  sendGet(url, _params, noLoading) {
+    const params = this._buildParams(_params);
+    return this.agent
+      .get(url, params, noLoading)
+      .then(json => this._handleData(json, url, params));
+  }
+  /**
+   * 显示提示信息，默认3秒
+   * @param {*提示内容} msg
+   */
+  showToast(msg) {
+    this.agent.showToast(msg);
+  }
+  /**
+   * 显示加载框，期间不可操作
+   * @param {*下面显示的提示内容，默认‘加载中’} msg
+   */
+  showLoading(msg) {
+    this.agent.showLoading(msg);
+  }
+  hideLoading() {
+    this.agent.hideLoading();
+  }
+  /**
+   * 构建参数
+   * @param {*参数} par
+   */
+  _buildParams(par) {
+    const params = { ...par };
+    //某些接口希望不传token，就需要删除token字段，默认都带token
+    if (params.noToken) {
+      delete params.noToken;
+      delete params.token;
+    } else {
+      /*如果没有this.UIStore.token就不代token，
             如果有params.token就不将this.UIStore.token覆盖它，
             如果有this.UIStore.token而没有params.token，就将this.UIStore.token付个params.token
             */
-            this.UIStore.token&&(params.token= params.token ?params.token:this.UIStore.token);
-        }
-        return params;
+      this.UIStore.token &&
+        (params.token = params.token ? params.token : this.UIStore.token);
     }
-    /**
-     * 处理获取的结果（主要是为了实现token自动刷新功能）
-     * @param {*获取到的结果} json 
-     */
-    _handleData(json,url,params){
-        switch (json.result) {
-            //获取数据成功
-            case '0':
-            case 'true':
-                return json; 
-            default:
-                console.log(`Requst is get Error,Code :${json.result}`);
-                const msg= ResponseCode.showMsg(json.result);
-                msg&& this.showToast(msg);
-                return json;
-            }
+    return params;
+  }
+  /**
+   * 处理获取的结果（主要是为了实现token自动刷新功能）
+   * @param {*获取到的结果} json
+   */
+  _handleData(json, url, params) {
+    if (!json || !json.result) return {};
+    switch (json.result) {
+      //这里可以根据接口实际情况修改
+      case '0':
+        return json;
+      /**
+       * 如果需要自动刷新token 请在这里写
+       */
+      // case '-1':
+      // return this.UIStore.refreshToken(url,params);
+      default:
+        console.log(`Requst is get Error,Code :${json.result}`);
+        const msg = ResponseCode.showMsg(json.result);
+        msg && this.showToast(msg);
+        return json;
     }
-} 
+  }
+}
 export default new RootStore();
