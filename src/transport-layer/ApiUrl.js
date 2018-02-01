4f014c93303f4773359bd63e2697d27d82de5adc
Huang Li <huangliop@163.com>
2018-02-01 11:50:15 +0800

    为接口添加一个根目录

--- src/transport-layer/ApiUrl.js
+++ src/transport-layer/ApiUrl.js
@@ -1,5 +1,5 @@
 //发布后的相对根目录
-const ROOT=`${process.env.PUBLIC_URL}`; 
+const ROOT=`${process.env.PUBLIC_URL}/api`; 
 const urls = { 
     LOGIN: '/login',//密码登录   示例代码，可以删除
     REGISTER:'/login',//用户注册  示例代码，可以删除
