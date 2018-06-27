24565f0b5d76c943ba60b71d0e58a35339291575
黄黎 <huangli@changan.com>
2018-06-27 11:44:05 +0800

    修改版本号

--- config-overrides.js
+++ config-overrides.js
@@ -46,8 +46,7 @@ module.exports = function override(config, env) {
         ]
       }
     );
-    config = rewireEslint(config, env,()=>{
-return {configFile:"src/.eslintrc",fix:true}
+    config = rewireEslint(config, env,()=>{return {configFile:"src/.eslintrc",fix:true}
 });
     return config;
   };
