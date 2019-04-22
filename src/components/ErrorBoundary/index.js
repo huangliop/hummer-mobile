26eaf07d26f3dbe87390a94da497ccd04e58e601
黄黎 <huangli@changan.com>
2019-04-22 12:22:54 +0800

    修改打印错误的位置

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -14,6 +14,7 @@ class ErrorBoundary extends Component {
     }
 
     static getDerivedStateFromError(error) {
+        console.error(error);
         // Update state so the next render will show the fallback UI.
         return { hasError: true };
     }
