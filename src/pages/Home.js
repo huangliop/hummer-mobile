1f15f8093c4e696c1ef4da3a85aa3b6a8ed5ef95
黄黎 <huangli@changan.com>
2019-05-13 14:05:20 +0800

    update to 3.0.1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,6 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
-
-function Example() {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        window.document.title = count;
-        return () => {
-            console.log('diss');
-        };
-    });
-    return (
-        <div>
-            <p>你点了{count}下了</p>
-            <button onClick={() => setCount(count + 1)}>点我</button>
-        </div>
-    );
-}
 
 class Home extends Component {
     render() {
@@ -27,7 +11,6 @@ class Home extends Component {
                 <Link to="/login">
                     <div>登录</div>
                 </Link>
-                <Example />
             </div>
         );
     }
